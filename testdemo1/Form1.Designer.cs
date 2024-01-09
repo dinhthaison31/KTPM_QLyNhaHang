c68e42b08fb6b59a21696663db17f33cf8bb9b32
dinhthaison31 <dinhthaison31@gmail.com>
2024-01-09 09:40:14 +0700

    update 9/1

---
+++ testdemo1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace testdemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_TestLogin = new System.Windows.Forms.Button();
+            this.btn_TestTable = new System.Windows.Forms.Button();
+            this.btn_TestOrder = new System.Windows.Forms.Button();
+            this.btn_TestServices = new System.Windows.Forms.Button();
+            this.btn_TestProfile = new System.Windows.Forms.Button();
+            this.btn_TestRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_TestLogin
+            // 
+            this.btn_TestLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestLogin.Location = new System.Drawing.Point(39, 47);
+            this.btn_TestLogin.Name = "btn_TestLogin";
+            this.btn_TestLogin.Size = new System.Drawing.Size(154, 45);
+            this.btn_TestLogin.TabIndex = 0;
+            this.btn_TestLogin.Text = "Test Đăng Nhập";
+            this.btn_TestLogin.UseVisualStyleBackColor = true;
+            this.btn_TestLogin.Click += new System.EventHandler(this.btn_TestLogin_Click);
+            // 
+            // btn_TestTable
+            // 
+            this.btn_TestTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestTable.Location = new System.Drawing.Point(39, 251);
+            this.btn_TestTable.Name = "btn_TestTable";
+            this.btn_TestTable.Size = new System.Drawing.Size(154, 45);
+            this.btn_TestTable.TabIndex = 0;
+            this.btn_TestTable.Text = "Test Đặt bàn";
+            this.btn_TestTable.UseVisualStyleBackColor = true;
+            this.btn_TestTable.Click += new System.EventHandler(this.btn_TestLogin_Click);
+            // 
+            // btn_TestOrder
+            // 
+            this.btn_TestOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestOrder.Location = new System.Drawing.Point(39, 200);
+            this.btn_TestOrder.Name = "btn_TestOrder";
+            this.btn_TestOrder.Size = new System.Drawing.Size(154, 45);
+            this.btn_TestOrder.TabIndex = 0;
+            this.btn_TestOrder.Text = "Test Đặt món";
+            this.btn_TestOrder.UseVisualStyleBackColor = true;
+            this.btn_TestOrder.Click += new System.EventHandler(this.btn_TestLogin_Click);
+            // 
+            // btn_TestServices
+            // 
+            this.btn_TestServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestServices.Location = new System.Drawing.Point(39, 149);
+            this.btn_TestServices.Name = "btn_TestServices";
+            this.btn_TestServices.Size = new System.Drawing.Size(154, 45);
+            this.btn_TestServices.TabIndex = 0;
+            this.btn_TestServices.Text = "Test Dịch Vụ";
+            this.btn_TestServices.UseVisualStyleBackColor = true;
+            this.btn_TestServices.Click += new System.EventHandler(this.btn_TestLogin_Click);
+            // 
+            // btn_TestProfile
+            // 
+            this.btn_TestProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestProfile.Location = new System.Drawing.Point(39, 302);
+            this.btn_TestProfile.Name = "btn_TestProfile";
+            this.btn_TestProfile.Size = new System.Drawing.Size(154, 45);
+            this.btn_TestProfile.TabIndex = 0;
+            this.btn_TestProfile.Text = "Test TTCN";
+            this.btn_TestProfile.UseVisualStyleBackColor = true;
+            this.btn_TestProfile.Click += new System.EventHandler(this.btn_TestLogin_Click);
+            // 
+            // btn_TestRegister
+            // 
+            this.btn_TestRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestRegister.Location = new System.Drawing.Point(39, 98);
+            this.btn_TestRegister.Name = "btn_TestRegister";
+            this.btn_TestRegister.Size = new System.Drawing.Size(154, 45);
+            this.btn_TestRegister.TabIndex = 1;
+            this.btn_TestRegister.Text = "Test Đăng Ký";
+            this.btn_TestRegister.UseVisualStyleBackColor = true;
+            this.btn_TestRegister.Click += new System.EventHandler(this.btn_TestRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_TestRegister);
+            this.Controls.Add(this.btn_TestProfile);
+            this.Controls.Add(this.btn_TestServices);
+            this.Controls.Add(this.btn_TestOrder);
+            this.Controls.Add(this.btn_TestTable);
+            this.Controls.Add(this.btn_TestLogin);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form lệnh test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_TestLogin;
+        private System.Windows.Forms.Button btn_TestTable;
+        private System.Windows.Forms.Button btn_TestOrder;
+        private System.Windows.Forms.Button btn_TestServices;
+        private System.Windows.Forms.Button btn_TestProfile;
+        private System.Windows.Forms.Button btn_TestRegister;
+    }
+}
+
---
+++ testdemo1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace testdemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_TestLogin_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            try
+            {
+                // Mở trình duyệt và điều hướng đến trang web cần đăng nhập
+                driver.Navigate().GoToUrl("https://stage.moosocial.com/php81/moo-320/"); // Thay thế bằng URL của trang web bạn muốn đăng nhập
+
+                // Tìm và click vào phần tử để mở form đăng nhập (ví dụ: button hoặc link)
+                IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"UserLoginForm\"]/div[2]/div[2]/input")); // Thay thế bằng XPath hoặc CSS Selector của nút đăng nhập
+                loginButton.Click();
+
+                // Nhập thông tin đăng nhập (ví dụ: tên đăng nhập và mật khẩu)
+                IWebElement usernameField = driver.FindElement(By.Id("login_email")); // Thay thế bằng id hoặc selector của trường tên đăng nhập
+                usernameField.SendKeys("user1@socialloft.com");
+
+                IWebElement passwordField = driver.FindElement(By.Id("login_password")); // Thay thế bằng id hoặc selector của trường mật khẩu
+                passwordField.SendKeys("");
+
+                //Đóng màn hình đen khi chạy chương trình
+                ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+                chrome.HideCommandPromptWindow = true;
+
+                // Submit form để đăng nhập
+                IWebElement submitButton = driver.FindElement(By.XPath("//input[@type='submit']")); // Thay thế bằng XPath hoặc CSS Selector của nút submit
+                submitButton.Click();
+
+                MessageBox.Show("Chức năng đăng nhập đạt yêu cầu!");
+            }
+            catch (NoSuchElementException ex)
+            {
+                MessageBox.Show("Không tìm thấy phần tử: " + ex.Message);
+            }
+            finally
+            {
+                // Đóng trình duyệt sau khi hoàn thành
+                driver.Quit();
+            }
+        }
+
+        private void btn_TestRegister_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            try
+            {
+                // Điều hướng đến trang đăng ký nhân viên của trang web
+                driver.Navigate().GoToUrl("https://example.com/register"); // Thay thế bằng URL của trang đăng ký nhân viên
+
+                // Điền thông tin đăng ký
+                driver.FindElement(By.Id("email")).SendKeys("test@example.com");
+                driver.FindElement(By.Id("name")).SendKeys("Test User");
+                driver.FindElement(By.Id("phone")).SendKeys("123456789");
+                driver.FindElement(By.Id("birthday")).SendKeys("2000-01-01");
+                driver.FindElement(By.Id("password")).SendKeys("testpassword");
+                driver.FindElement(By.Id("confirm_password")).SendKeys("testpassword");
+
+                // Upload ảnh (đường dẫn file ảnh cần thay đổi theo đường dẫn thực tế)
+                string imagePath = @"C:\path\to\image.jpg";
+                if (File.Exists(imagePath))
+                {
+                    driver.FindElement(By.Id("image_upload")).SendKeys(imagePath);
+                }
+
+                // Submit form đăng ký
+                driver.FindElement(By.Id("register_button")).Click();
+
+                MessageBox.Show("Đăng ký nhân viên thành công!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+        }
+    }
+}
