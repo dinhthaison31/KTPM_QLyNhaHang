7cec29d6a1982792eae1ba44f65107c007b89f69
manhcuong1909 <nguyenmanhcuong19092001@gmail.com>
2024-01-14 22:55:36 +0700

    update 14/1

--- testdemo1/Form1.Designer.cs
+++ testdemo1/Form1.Designer.cs
@@ -33,17 +33,19 @@
             this.btn_TestRegister = new System.Windows.Forms.Button();
             this.btn_testbooking = new System.Windows.Forms.Button();
             this.btn_testfoodorder = new System.Windows.Forms.Button();
-            this.btn_TestService = new System.Windows.Forms.Button();
+            this.btn_TestCreateStaff = new System.Windows.Forms.Button();
             this.btn_TestProfile = new System.Windows.Forms.Button();
+            this.btn_TestServices = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_TestLogin
             // 
             this.btn_TestLogin.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btn_TestLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_TestLogin.Location = new System.Drawing.Point(36, 498);
+            this.btn_TestLogin.Location = new System.Drawing.Point(48, 613);
+            this.btn_TestLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_TestLogin.Name = "btn_TestLogin";
-            this.btn_TestLogin.Size = new System.Drawing.Size(138, 34);
+            this.btn_TestLogin.Size = new System.Drawing.Size(184, 42);
             this.btn_TestLogin.TabIndex = 0;
             this.btn_TestLogin.Text = "Test Đăng Nhập";
             this.btn_TestLogin.UseVisualStyleBackColor = false;
@@ -53,9 +55,10 @@
             // 
             this.btn_TestRegister.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btn_TestRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_TestRegister.Location = new System.Drawing.Point(175, 498);
+            this.btn_TestRegister.Location = new System.Drawing.Point(233, 613);
+            this.btn_TestRegister.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_TestRegister.Name = "btn_TestRegister";
-            this.btn_TestRegister.Size = new System.Drawing.Size(138, 34);
+            this.btn_TestRegister.Size = new System.Drawing.Size(184, 42);
             this.btn_TestRegister.TabIndex = 1;
             this.btn_TestRegister.Text = "Test Đăng Ký";
             this.btn_TestRegister.UseVisualStyleBackColor = false;
@@ -65,10 +68,10 @@
             // 
             this.btn_testbooking.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btn_testbooking.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_testbooking.Location = new System.Drawing.Point(313, 498);
-            this.btn_testbooking.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_testbooking.Location = new System.Drawing.Point(417, 613);
+            this.btn_testbooking.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_testbooking.Name = "btn_testbooking";
-            this.btn_testbooking.Size = new System.Drawing.Size(138, 34);
+            this.btn_testbooking.Size = new System.Drawing.Size(184, 42);
             this.btn_testbooking.TabIndex = 2;
             this.btn_testbooking.Text = "Test Đặt Bàn";
             this.btn_testbooking.UseVisualStyleBackColor = false;
@@ -78,54 +81,69 @@
             // 
             this.btn_testfoodorder.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btn_testfoodorder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_testfoodorder.Location = new System.Drawing.Point(450, 498);
-            this.btn_testfoodorder.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_testfoodorder.Location = new System.Drawing.Point(600, 613);
+            this.btn_testfoodorder.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_testfoodorder.Name = "btn_testfoodorder";
-            this.btn_testfoodorder.Size = new System.Drawing.Size(138, 34);
+            this.btn_testfoodorder.Size = new System.Drawing.Size(184, 42);
             this.btn_testfoodorder.TabIndex = 3;
             this.btn_testfoodorder.Text = "Test Đặt Món";
             this.btn_testfoodorder.UseVisualStyleBackColor = false;
             this.btn_testfoodorder.Click += new System.EventHandler(this.btn_testfoodorder_Click);
             // 
-            // btn_TestService
+            // btn_TestCreateStaff
             // 
-            this.btn_TestService.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btn_TestService.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_TestService.Location = new System.Drawing.Point(593, 498);
-            this.btn_TestService.Name = "btn_TestService";
-            this.btn_TestService.Size = new System.Drawing.Size(138, 33);
-            this.btn_TestService.TabIndex = 4;
-            this.btn_TestService.Text = "Test Dịch Vụ";
-            this.btn_TestService.UseVisualStyleBackColor = false;
-            this.btn_TestService.Click += new System.EventHandler(this.btn_TestService_Click);
+            this.btn_TestCreateStaff.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_TestCreateStaff.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestCreateStaff.Location = new System.Drawing.Point(791, 613);
+            this.btn_TestCreateStaff.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_TestCreateStaff.Name = "btn_TestCreateStaff";
+            this.btn_TestCreateStaff.Size = new System.Drawing.Size(184, 41);
+            this.btn_TestCreateStaff.TabIndex = 4;
+            this.btn_TestCreateStaff.Text = "Test QLNV";
+            this.btn_TestCreateStaff.UseVisualStyleBackColor = false;
+            this.btn_TestCreateStaff.Click += new System.EventHandler(this.btn_TestCreateStaff_Click_1);
             // 
             // btn_TestProfile
             // 
             this.btn_TestProfile.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.btn_TestProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_TestProfile.Location = new System.Drawing.Point(737, 498);
+            this.btn_TestProfile.Location = new System.Drawing.Point(983, 613);
+            this.btn_TestProfile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_TestProfile.Name = "btn_TestProfile";
-            this.btn_TestProfile.Size = new System.Drawing.Size(138, 32);
+            this.btn_TestProfile.Size = new System.Drawing.Size(184, 39);
             this.btn_TestProfile.TabIndex = 5;
             this.btn_TestProfile.Text = "Test TTCN";
             this.btn_TestProfile.UseVisualStyleBackColor = false;
             this.btn_TestProfile.Click += new System.EventHandler(this.btn_TestProfile_Click);
             // 
+            // btn_TestServices
+            // 
+            this.btn_TestServices.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TestServices.Location = new System.Drawing.Point(48, 549);
+            this.btn_TestServices.Name = "btn_TestServices";
+            this.btn_TestServices.Size = new System.Drawing.Size(184, 43);
+            this.btn_TestServices.TabIndex = 6;
+            this.btn_TestServices.Text = "Test QLDV";
+            this.btn_TestServices.UseVisualStyleBackColor = true;
+            this.btn_TestServices.Click += new System.EventHandler(this.btn_TestServices_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(901, 553);
+            this.ClientSize = new System.Drawing.Size(1184, 675);
+            this.Controls.Add(this.btn_TestServices);
             this.Controls.Add(this.btn_TestProfile);
-            this.Controls.Add(this.btn_TestService);
+            this.Controls.Add(this.btn_TestCreateStaff);
             this.Controls.Add(this.btn_testfoodorder);
             this.Controls.Add(this.btn_testbooking);
             this.Controls.Add(this.btn_TestRegister);
             this.Controls.Add(this.btn_TestLogin);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form lệnh test";
@@ -139,8 +157,9 @@
         private System.Windows.Forms.Button btn_TestRegister;
         private System.Windows.Forms.Button btn_testbooking;
         private System.Windows.Forms.Button btn_testfoodorder;
-        private System.Windows.Forms.Button btn_TestService;
+        private System.Windows.Forms.Button btn_TestCreateStaff;
         private System.Windows.Forms.Button btn_TestProfile;
+        private System.Windows.Forms.Button btn_TestServices;
     }
 }
 
--- testdemo1/Form1.cs
+++ testdemo1/Form1.cs
@@ -12,6 +12,7 @@ using System.Windows.Forms;
 using System.IO;
 using OpenQA.Selenium.Support.UI;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using System.Globalization;
 
 namespace testdemo1
 {
@@ -38,7 +39,7 @@ namespace testdemo1
                 loginButton.Click();
 
                 // Nhập thông tin đăng nhập (ví dụ: tên đăng nhập và mật khẩu)
-                IWebElement usernameField = driver.FindElement(By.Id("email")); 
+                IWebElement usernameField = driver.FindElement(By.Id("email"));
                 usernameField.SendKeys("abcd123@gmail.com");
 
                 IWebElement passwordField = driver.FindElement(By.Id("password"));
@@ -76,13 +77,13 @@ namespace testdemo1
                 driver.Navigate().GoToUrl("http://localhost:8080/QuanLyNhaHang/register"); // Thay thế bằng URL của trang đăng ký nhân viên
 
                 // Điền thông tin đăng ký
-                driver.FindElement(By.Id("email")).SendKeys("abcd12@gmail.com");
-                driver.FindElement(By.Id("name")).SendKeys("Tu nhi");
-                driver.FindElement(By.Id("phone")).SendKeys("0912333345");
+                driver.FindElement(By.Id("email")).SendKeys("aaa123@gmail.com");
+                driver.FindElement(By.Id("name")).SendKeys("manhcuong1");
+                driver.FindElement(By.Id("phone")).SendKeys("0923226323");
                 // Tìm trường nhập ngày sinh
                 IWebElement birthDayInput = driver.FindElement(By.Id("birth-day"));
                 // Sử dụng JavaScript để thiết lập giá trị cho trường ngày
-                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value = arguments[1];", birthDayInput, "2001-10-20");
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value = arguments[1];", birthDayInput, "2000-09-19");
 
                 //  driver.FindElement(By.Id("birth-day")).SendKeys("20-Oct-2001");
 
@@ -97,12 +98,9 @@ namespace testdemo1
                     driver.FindElement(By.Id("imageId")).SendKeys(imagePath);
                 }
 
-                // Submit form đăng ký
-                // driver.FindElement(By.Id("submit")).Click();
 
                 IWebElement registerButton = driver.FindElement(By.XPath("//*[@id=\"user\"]/input"));
                 registerButton.Click();
-
                 MessageBox.Show("Đăng ký người dùng thành công!");
             }
             catch (Exception ex)
@@ -136,17 +134,17 @@ namespace testdemo1
                 IWebElement bookingButton = driver.FindElement(By.LinkText("Booking"));
                 bookingButton.Click();
 
-                // Tìm trường nhập ngày sinh
+                // Tìm trường nhập ngày 
                 IWebElement eventDate = driver.FindElement(By.Id("date"));
                 // Sử dụng JavaScript để thiết lập giá trị cho trường ngày
                 ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value = arguments[1];", eventDate, "2024-01-29");
-
+                // Chọn thời gian trong ngày 
                 SelectElement timeDropdown = new SelectElement(driver.FindElement(By.Id("type_day")));
                 timeDropdown.SelectByText("affternoon");
-
+                // Nhập số bàn
                 IWebElement tableField = driver.FindElement(By.Id("countTable"));
-                tableField.SendKeys("25");
-
+                tableField.SendKeys("35");
+                // Ghi chú
                 IWebElement noteField = driver.FindElement(By.Id("note"));
                 noteField.SendKeys("12g ");
 
@@ -156,21 +154,21 @@ namespace testdemo1
                 //sanh rose
                 IWebElement roseHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[1]/div/button"));
                 roseHallSelect.Click();
-                //sanh Camellia
-                IWebElement camelliaHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[3]/div/button"));
-                camelliaHallSelect.Click();
-                //sanh Tulip
-                IWebElement tulipHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[4]/div/button"));
-                tulipHallSelect.Click();
-                //sanh Lotus
-                IWebElement lotusHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[5]/div/button"));
-                tulipHallSelect.Click();
-                //sanh Pense
-                IWebElement penseHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[6]/div/button"));
-                penseHallSelect.Click();
-                //sanh Violet
-                IWebElement violetHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[7]/div/button"));
-                violetHallSelect.Click();
+                ////sanh Camellia
+                //IWebElement camelliaHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[3]/div/button"));
+                //camelliaHallSelect.Click();
+                ////sanh Tulip
+                //IWebElement tulipHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[4]/div/button"));
+                //tulipHallSelect.Click();
+                ////sanh Lotus
+                //IWebElement lotusHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[5]/div/button"));
+                //tulipHallSelect.Click();
+                ////sanh Pense
+                //IWebElement penseHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[6]/div/button"));
+                //penseHallSelect.Click();
+                ////sanh Violet
+                //IWebElement violetHallSelect = driver.FindElement(By.XPath("//*[@id=\"exampleModal\"]/div/div/div[2]/div[7]/div/button"));
+                //violetHallSelect.Click();
             }
             catch (Exception ex)
             {
@@ -204,13 +202,13 @@ namespace testdemo1
                 IWebElement eventDate = driver.FindElement(By.Id("date"));
                 // Sử dụng JavaScript để thiết lập giá trị cho trường ngày
                 ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value = arguments[1];", eventDate, "2024-01-29");
-
+                //Thời gian trong ngày
                 SelectElement timeDropdown = new SelectElement(driver.FindElement(By.Id("type_day")));
                 timeDropdown.SelectByText("affternoon");
-
+                // Số bàn muốn đặt
                 IWebElement tableField = driver.FindElement(By.Id("countTable"));
                 tableField.SendKeys("25");
-
+                // Ghi chú
                 IWebElement noteField = driver.FindElement(By.Id("note"));
                 noteField.SendKeys("12g ");
 
@@ -223,23 +221,29 @@ namespace testdemo1
                 
                 // Chọn món bất kì, thay đổi tên biến, đường dẫn xpath, số lần click trong vòng lặp sau đó copy paste
                 // Viết thêm nếu muốn chọn thêm những món khác
-                IWebElement randomDishes = driver.FindElement(By.XPath(""));
-                for (int i = 0; i < 5; i++)
-                {
-                    randomDishes.Click();
-                }
+                //IWebElement randomDishes = driver.FindElement(By.XPath(""));
+                //for (int i = 0; i < 5; i++)
+                //{
+                //    randomDishes.Click();
+                //}
+
 
                 // VD: Chọn món cá gì đó
-                IWebElement fishDishes = driver.FindElement(By.XPath(""));
+                IWebElement monchinhmotDishes = driver.FindElement(By.XPath("//*[@id=\"btn1\"]/button"));
                 for (int i = 0; i < 3; i++)
                 {
-                    fishDishes.Click();
+                    monchinhmotDishes.Click();
                 }
+                
 
-                // ...
 
                 // Nhấn nút tiếp tục để tiến hành thanh toán
-                IWebElement continueButton = driver.FindElement(By.XPath(""));
+                //IWebElement continueButton = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div[2]/div[2]/div/div/div[1]/a"));
+                //continueButton.Click();
+
+                IWebElement continueButton = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div[2]/div[2]/div/div/div[1]/a"));
+                driver.Navigate().Refresh();
+                continueButton = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/div[2]/div[2]/div/div/div[1]/a"));
                 continueButton.Click();
 
                 // Tiến hành thanh toán chọn 1 trong 2 options dưới đây để chạy
@@ -262,14 +266,157 @@ namespace testdemo1
             }
         }
 
-        private void btn_TestService_Click(object sender, EventArgs e)
+      
+        //Test Thông tin cá nhân
+        private void btn_TestProfile_Click(object sender, EventArgs e)
         {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost:8080/QuanLyNhaHang/login");
+
+                IWebElement usernameField = driver.FindElement(By.Id("email"));
+                usernameField.SendKeys("abcd123@gmail.com");
+
+                IWebElement passwordField = driver.FindElement(By.Id("password"));
+                passwordField.SendKeys("Admin@123");
+
+                IWebElement loginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/form/input"));
+                loginButton.Click();
+
+                IWebElement avatarDropdown = driver.FindElement(By.XPath("//*[@id=\"mynavbar\"]/ul[2]/li[2]/a"));
+                avatarDropdown.Click();
+
+                IWebElement profileLink = driver.FindElement(By.XPath("//*[@id=\"mynavbar\"]/ul[2]/li[2]/ul/li[1]/a"));
+                profileLink.Click();
+                //Tên
+                IWebElement renameField = driver.FindElement(By.Id("name"));
+                renameField.Clear();
+                renameField.SendKeys("thai son");
+                // Số điện thoại
+                IWebElement phoneField = driver.FindElement(By.XPath("//*[@id=\"mobile\"]"));
+                phoneField.Clear();
+                phoneField.SendKeys("");
+              
+                // Tìm trường nhập ngày sinh
+               IWebElement birthdayDate = driver.FindElement(By.Id("birthday"));
+                // Sử dụng JavaScript để thiết lập giá trị cho trường ngày
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value = arguments[1];", birthdayDate, "2002-12-01");
+
+                
+
+                IWebElement saveButton = driver.FindElement(By.XPath("//*[@id=\"profileUser\"]/input[2]"));
+                saveButton.Click();
+            }
+            catch 
+                (Exception ex)
+            {
+                    MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+                }
 
         }
 
-        private void btn_TestProfile_Click(object sender, EventArgs e)
+        // Test Nhân Viên
+        private void btn_TestCreateStaff_Click_1(object sender, EventArgs e)
         {
 
+            IWebDriver driver = new ChromeDriver();
+
+            try
+            {
+                driver.Navigate().GoToUrl("http://localhost:8080/QuanLyNhaHang/login");
+
+                IWebElement usernameField = driver.FindElement(By.Id("email"));
+                usernameField.SendKeys("abc123@gmail.com");
+
+                IWebElement passwordField = driver.FindElement(By.Id("password"));
+                passwordField.SendKeys("Admin@123");
+
+                IWebElement loginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/form/input"));
+                loginButton.Click();
+
+                IWebElement staffmanageButton = driver.FindElement(By.XPath("//*[@id=\"mynavbar\"]/ul[1]/li[5]/a/b"));
+                staffmanageButton.Click();
+
+                IWebElement addstaffButton = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div/table/thead/tr[1]/th[2]/button"));
+                addstaffButton.Click();
+
+                IWebElement namestaffField = driver.FindElement(By.Id("nameStaff"));
+                namestaffField.SendKeys("manhcuong a 1");
+
+                // Tìm trường nhập ngày sinh
+                IWebElement birthdaystaffDate = driver.FindElement(By.Id("birthdayStaff"));
+                // Sử dụng JavaScript để thiết lập giá trị cho trường ngày
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value = arguments[1];", birthdaystaffDate, "2001-01-24");
+
+                IWebElement phonestaffField = driver.FindElement(By.XPath("//*[@id=\"mobileStaff\"]"));
+                phonestaffField.SendKeys("0906440088");
+
+                IWebElement emailstaffField = driver.FindElement(By.Id("emailStaff"));
+                emailstaffField.SendKeys("abb123@gmail.com");
+
+                IWebElement savestaffButton = driver.FindElement(By.XPath("//*[@id=\"newStaff\"]/div[3]/button[2]"));
+                savestaffButton.Click();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+
+
+        }
+
+        // Test Quan ly dich vu
+        private void btn_TestServices_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            try {
+                driver.Navigate().GoToUrl("http://localhost:8080/QuanLyNhaHang/login");
+
+                IWebElement usernameField = driver.FindElement(By.Id("email"));
+                usernameField.SendKeys("abc123@gmail.com");
+
+                IWebElement passwordField = driver.FindElement(By.Id("password"));
+                passwordField.SendKeys("Admin@123");
+
+                IWebElement loginButton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div/div[2]/form/input"));
+                loginButton.Click();
+
+                IWebElement servicemanageButton = driver.FindElement(By.XPath("//*[@id=\"mynavbar\"]/ul[1]/li[4]/a"));
+                servicemanageButton.Click();
+                // Them dịch vụ
+                IWebElement addserviceButton = driver.FindElement(By.XPath("/html/body/div[2]/div[1]/button"));
+                addserviceButton.Click();
+                //Thêm thông tin của dịch vụ
+                //IWebElement nameserviceField = driver.FindElement(By.Id("nameService"));
+                //nameserviceField.SendKeys("Bóng bay");
+
+                //IWebElement servicepriceField = driver.FindElement(By.Id("price"));
+                //servicepriceField.Clear();
+                //servicepriceField.SendKeys("1000");
+
+                //IWebElement servicedesField = driver.FindElement(By.Id("describe"));
+                //servicedesField.SendKeys("Thổi bóng bay");
+
+                //Upload ảnh(đường dẫn file ảnh cần thay đổi theo đường dẫn thực tế)
+                //string imagePath = @"C:\Users\nguye\Downloads\36961409-quả-bóng-màu-đỏ-bị-cô-lập-trên-nền-trắng.jpg";
+                //if (File.Exists(imagePath))
+                //{
+                //    driver.FindElement(By.Id("imageUpload")).SendKeys(imagePath);
+                //}
+
+                IWebElement saveserviceButton = driver.FindElement(By.XPath("//*[@id=\"newService\"]/div[3]/button[2]"));
+                saveserviceButton.Click();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
         }
     }
 }
